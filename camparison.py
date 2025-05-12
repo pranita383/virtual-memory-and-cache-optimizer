50bd6621694e04414bb7de52499fd7d53bf16dde
lokeshbankoti <lokeshsinghbankoti86@gmail.com>
2025-05-12 07:28:01 +0530

    Create camparison.py

---
+++ camparison.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from simulate_before import simulate_before
+from simulate_after import simulate_after
+
+app = Flask(__name__)
+
+@app.route('/api/comparison', methods=['GET'])
+def compare_memory_usage():
+    page_faults_before = simulate_before()
+    page_faults_after = simulate_after()
+    
+    comparison_data = {
+        "before_optimization": page_faults_before,
+        "after_optimization": page_faults_after
+    }
+    
+    return jsonify(comparison_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
